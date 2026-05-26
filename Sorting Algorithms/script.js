3eba0e10b8e6bec4e1d0c528dd647229965471d2
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-26 21:17:33 +0530

    Complete the Bubble sort

--- Sorting Algorithms/script.js
+++ Sorting Algorithms/script.js
@@ -23,7 +23,6 @@ function selectionSort(arr) {
 }
 
 // selectionSort([12, 2, 15, 8, 20, 80, 1, 0]);
-
 // T.C = O(n^2)
 // S.C = O(1)
 
@@ -46,4 +45,27 @@ function insertionSort(arr) {
     console.log("Insertion Sorted Array", arr);
 }
 
-insertionSort([12, 2, 15, 8, 20, 80, 1, 0]);
+// insertionSort([12, 2, 15, 8, 20, 80, 1, 0]);
+// T.C = O(n^2)
+// S.C = O(1)
+
+// 3. Bubble Sort
+
+function bubbleSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j + 1] < arr[j]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    console.log("Bubble Sorted Array", arr);
+}
+
+// bubbleSort([12, 2, 15, 8, 20, 80, 1, 0]);
+// T.C = O(n^2)
+// S.C = O(1)
