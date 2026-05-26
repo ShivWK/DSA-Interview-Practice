3d54d4583c104ba79cee0d7906997478b7d918b5
Shivendra Dwivedi <shivendrawk@gmail.com>
2026-05-26 20:13:32 +0530

    Complete the Insertion sort

---
+++ Sorting Algorithms/script.js
@@ -0,0 +1,49 @@
+console.log("Sorting Algorithms");
+
+// 1. Selection Sort
+
+function selectionSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < (n - 1); i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        const temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    console.log("Selection Sorted Array", arr)
+}
+
+// selectionSort([12, 2, 15, 8, 20, 80, 1, 0]);
+
+// T.C = O(n^2)
+// S.C = O(1)
+
+// 2. Insertion Sort
+
+function insertionSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n; i++) {
+        let j = i;
+
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            const temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    console.log("Insertion Sorted Array", arr);
+}
+
+insertionSort([12, 2, 15, 8, 20, 80, 1, 0]);
